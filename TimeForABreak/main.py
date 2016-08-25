afd5b374544bdf19fbdc28af0f66fe7db278e740
Nikos Katsos <nikoskeyz@gmail.com>
2016-08-25 20:37:10 +0300

    fixed indentation

--- TimeForABreak/main.py
+++ TimeForABreak/main.py
@@ -8,8 +8,8 @@ breaks_count = 0
 
 while(breaks_count < breaks_limit):
     time.sleep(2*60*60)
-	print(time.ctime())
-	print("Come on... It's time for a break. Here's your favorite song...")
+    print(time.ctime())
+    print("Come on... It's time for a break. Here's your favorite song...")
     webbrowser.open("https://www.youtube.com/watch?v=chmCtZTSUWI")
     breaks_count += 1
 
